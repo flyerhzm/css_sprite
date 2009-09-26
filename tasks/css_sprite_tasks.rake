e5c073cd040fd9a976e5f3ec988825538366a6be
Richard Huang <flyerhzm@gmail.com>
2009-09-26 23:13:27 +0800

    first commit

---
+++ tasks/css_sprite_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :css_sprite do
+#   # Task goes here
+# end
---
+++ test/css_sprite_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class CssSpriteTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
