f9ca33c35ed38a77d4347074fc0f3ab9986d05c4
Richard Huang <flyerhzm@gmail.com>
2010-01-25 22:27:15 +0800

    fix spec

--- spec/css_sprite/sprite_spec.rb
+++ spec/css_sprite/sprite_spec.rb
@@ -1,34 +1,34 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
-describe CssSprite do
+describe Sprite do
   before(:all) do
-    @sprite = CssSprite.new
+    @sprite = Sprite.new
   end
   
   context "get_image" do
     it "should get a image" do
-      @sprite.get_image(File.join(File.dirname(__FILE__), 'resources/good_topic.gif')).class.should == Magick::Image
+      @sprite.get_image(File.join(File.dirname(__FILE__), '../resources/good_topic.gif')).class.should == Magick::Image
     end
   end
   
   context "image_properties" do
     it "should get image properties" do
-      image = @sprite.get_image(File.join(File.dirname(__FILE__), 'resources/good_topic.gif'))
+      image = @sprite.get_image(File.join(File.dirname(__FILE__), '../resources/good_topic.gif'))
       @sprite.image_properties(image).should == {:name => 'good_topic', :width => 20, :height => 19}
     end
   end
   
   context "composite_images" do
     it "should composite two images into one horizontally" do
-      image1 = @sprite.get_image(File.join(File.dirname(__FILE__), 'resources/good_topic.gif'))
-      image2 = @sprite.get_image(File.join(File.dirname(__FILE__), 'resources/mid_topic.gif'))
+      image1 = @sprite.get_image(File.join(File.dirname(__FILE__), '../resources/good_topic.gif'))
+      image2 = @sprite.get_image(File.join(File.dirname(__FILE__), '../resources/mid_topic.gif'))
       image = @sprite.composite_images(image1, image2, image1.columns, 0)
       @sprite.image_properties(image).should == {:name => nil, :width => 40, :height => 19}
     end
     
     it "should composite two images into one verically" do
-      image1 = @sprite.get_image(File.join(File.dirname(__FILE__), 'resources/good_topic.gif'))
-      image2 = @sprite.get_image(File.join(File.dirname(__FILE__), 'resources/mid_topic.gif'))
+      image1 = @sprite.get_image(File.join(File.dirname(__FILE__), '../resources/good_topic.gif'))
+      image2 = @sprite.get_image(File.join(File.dirname(__FILE__), '../resources/mid_topic.gif'))
       image = @sprite.composite_images(image1, image2, 0, image1.rows)
       @sprite.image_properties(image).should == {:name => nil, :width => 20, :height => 38}
     end
