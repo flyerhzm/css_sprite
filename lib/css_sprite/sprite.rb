1ce418b16d59afc6261c73bb784c22237a4c8f7c
Richard Huang <flyerhzm@gmail.com>
2010-04-08 10:53:03 +0800

    use Magic::PaletteMatteType

--- lib/css_sprite/sprite.rb
+++ lib/css_sprite/sprite.rb
@@ -77,6 +77,7 @@ class Sprite
         results << image_properties(source_image, directory).merge(:x => x, :y => y)
         dest_image = composite_images(dest_image, source_image, x, y)
       end
+      dest_image.image_type = Magick::PaletteMatteType
       dest_image.write(dest_image_path)
     end
     results
